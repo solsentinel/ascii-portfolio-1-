39efecde39cf7c171f4b51255a85c508f0882169
solsentinel <solsentinel960@gmail.com>
2025-03-26 14:22:50 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -103,6 +103,19 @@ export default function Home() {
             
             <CrtToggle />
 
+            {/* X (Twitter) Button at top right */}
+            <a 
+              href="https://x.com/promixel143" 
+              target="_blank" 
+              rel="noopener noreferrer" 
+              className="fixed top-4 right-16 z-30 bg-black/80 p-2 rounded-md border border-white/10 hover:bg-black/60 transition-colors"
+              title="Follow us on X"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-white">
+                <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"></path>
+              </svg>
+            </a>
+
             {/* Login Button at top left */}
             <div className="fixed top-4 left-4 z-30">
               {user ? (
